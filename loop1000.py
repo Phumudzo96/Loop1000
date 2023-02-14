4bc02cde94c1ae57f36a11e6fa952d0cfab5ddac
Phumudzo <103152398+Phumudzo96@users.noreply.github.com>
2023-02-15 00:28:25 +0200

    Add files via upload

---
+++ loop1000.py
@@ -0,0 +1,10 @@
+i = 1
+for i in range(1,1001):      # set number to be in the range of 1 to 100
+    print(i)                 # print out the range of number
+
+i = 1
+for i in range(1,1001):      
+    if i % 2 == 0:            #  Get all the even numbers from 1 to 1000
+            print(i)          #  Print all the even numbers
+
+
